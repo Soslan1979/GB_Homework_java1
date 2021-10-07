2035c307eb27adda5a0509fd830e68479fd45882
geekbrains <info@geekbrains.ru>
2021-10-07 16:11:29 +0300

    Added changes to the file HomeworkLesson2

--- src/Homework/HomeworkLesson2.java
+++ src/Homework/HomeworkLesson2.java
@@ -3,47 +3,44 @@ package Homework;
 public class HomeworkLesson2 {
     public static void main(String[] args) {
         System.out.println(sum(5, 15));
+        System.out.println(sum(10, 15));
         checkPlusOrMinus(-10);
-        System.out.println(bool(7));
-        printString("HelloWorld!", 3);
+        checkPlusOrMinus(10);
+        System.out.println(checkTrueOrFalse(7));
+        System.out.println(checkTrueOrFalse(-7));
+        printString("Hello world!", 3);
+        printString("Hi guys", 5);
         System.out.println(leapYear(2021));
+        System.out.println(leapYear(2020));
     }
 
     static boolean sum(int a, int b) {
-        System.out.println("Задание №1");
         int c = a + b;
         if (c >= 10 && c <= 20) return true;
         return false;
     }
 
     static void checkPlusOrMinus(int a) {
-        System.out.println();
-        System.out.println("Задание №2");
         if (a >= 0) System.out.println(a + " это положительное число");
         else System.out.println(a + " это отрицательное число");
     }
 
-    static boolean bool(int a) {
-        System.out.println();
-        System.out.println("Задание №3");
+    static boolean checkTrueOrFalse(int a) {;
         if (a <= 0) return true;
         return false;
     }
 
     static void printString(String a, int b) {
-        System.out.println();
-        System.out.println("Задание №4");
         for (int i = 0; i < b; i++) {
             System.out.println(a);
-
         }
     }
+
     static boolean leapYear(int a) {
-        System.out.println();
-        System.out.println("Задание №5");
         if (a % 4 == 0)
             if (a % 100 != 0 || a % 400 == 0) return true;
         return false;
+
     }
 }
 
